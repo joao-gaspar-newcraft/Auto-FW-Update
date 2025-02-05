bf230823b52f4a2cf884de1c871614989c6e1a36
João-Gaspar-Newcraft <joao.gaspar@newcraft.pt>
2025-02-05 11:16:15 +0000

    Test commit 5

--- include/version.h
+++ include/version.h
@@ -2,9 +2,9 @@
 #define VERSION_H
 
 // Automatically updated version number
-#define VERSION "1.0.4"
+#define VERSION "1.0.5"
 
 // Timestamp of the last commit
-#define VERSION_TIMESTAMP "2025-02-05_11:06"
+#define VERSION_TIMESTAMP "2025-02-05_11:16"
 
 #endif // VERSION_H
