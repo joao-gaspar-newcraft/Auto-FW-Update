739b16576aca70931049872dcd3c95a4edc4dd05
João-Gaspar-Newcraft <joao.gaspar@newcraft.pt>
2025-02-05 10:53:14 +0000

    Pilot commit

---
+++ include/version.h
@@ -0,0 +1,10 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// Automatically updated version number
+#define VERSION "1.0.1"
+
+// Timestamp of the last commit
+#define VERSION_TIMESTAMP "2024-02-03_12:05"
+
+#endif // VERSION_H
