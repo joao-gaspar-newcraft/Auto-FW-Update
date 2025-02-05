d6d0eed6550a5882756bbdc5c90f5a92b35ed51b
João-Gaspar-Newcraft <joao.gaspar@newcraft.pt>
2025-02-05 11:28:41 +0000

    Enhance post-checkout hook to set hooks path and ensure pre-commit hook installation

--- include/version.h
+++ include/version.h
@@ -2,9 +2,9 @@
 #define VERSION_H
 
 // Automatically updated version number
-#define VERSION "1.0.5"
+#define VERSION "1.0.6"
 
 // Timestamp of the last commit
-#define VERSION_TIMESTAMP "2025-02-05_11:16"
+#define VERSION_TIMESTAMP "2025-02-05_11:28"
 
 #endif // VERSION_H
